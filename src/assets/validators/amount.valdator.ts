2ca44f5a5c899785587f450f43cd6e6e3cd90753
M.EJAZ UR RAHMAN <ejazsidhu@gmail.com>
2017-11-13 18:16:16 +0500

    first dommcit only bootstap  is setup

---
+++ src/assets/validators/amount.valdator.ts
@@ -0,0 +1,16 @@
+import { AbstractControl } from '@angular/forms';
+
+export class AmountValidator{
+
+    public static validate(c:AbstractControl){
+        let amount_regex=/^[0-9]*$/;
+
+
+        return amount_regex.test(c.value) ? null : {
+            validateEmail: {
+              valid: false
+            }
+          };
+    }
+
+}
