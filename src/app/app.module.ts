5ef5a4d8a77ef6340df9197f2dd2d06dd4e3063d
M.EJAZ UR RAHMAN <ejazsidhu@gmail.com>
2017-11-13 22:41:38 +0500

    adding user component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddUserComponent } from './pages/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule
