62b4524e9c30a395009d28b85f14b686bad8a50c
M.EJAZ UR RAHMAN <ejazsidhu@gmail.com>
2017-11-13 22:44:18 +0500

    all user component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AddUserComponent } from './pages/add-user/add-user.component';
 import { IncomeTransectionComponent } from './pages/income-transection/income-transection.component';
+import { AllUsersComponent } from './pages/all-users/all-users.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddUserComponent,
-    IncomeTransectionComponent
+    IncomeTransectionComponent,
+    AllUsersComponent
   ],
   imports: [
     BrowserModule
