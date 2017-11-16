1516054b435dda7033928cdf2e2979e162a111f4
thezeeshanasghar <thezeeshanasghar@gmail.com>
2017-11-16 18:09:58 +0500

    fix date issue in add account

--- src/app/Models/account.model.ts
+++ src/app/Models/account.model.ts
@@ -1,12 +1,12 @@
 export class AccountModel {
 
     public Id: number;
-    public Number: String;
+    public Number: string;
     public Name: string;
     public MobileNumber: string;
     public CNIC: string;
     public Address: string;
-    public Created: Date;
+    public Created: string;
     public Balance: number;
     public constructor() {}
 }
