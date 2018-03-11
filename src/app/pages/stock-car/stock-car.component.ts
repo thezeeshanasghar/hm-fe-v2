6c9529a26c0469bf0a12d0f1c1600a96046fead2
thezeeshanasghar <thezeeshanasghar@gmail.com>
2018-03-11 08:24:03 +0500

    remove unnessary code from ts

--- src/app/pages/stock-car/stock-car.component.ts
+++ src/app/pages/stock-car/stock-car.component.ts
@@ -1,68 +1,41 @@
 import { GeneralHttpService } from './../../services/general-http.service';
 import { AmountValidator } from './../../../assets/validators/amount.valdator';
-import { FormGroup, FormControl, FormBuilder, Validators, AbstractControl } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { AccountModel } from '../../Models/account.model';
 
 @Component({
   selector: 'app-stock-car',
   templateUrl: './stock-car.component.html'
-  
+
 })
 export class StockCarComponent implements OnInit {
 
-public form:FormGroup;
-public name:AbstractControl;
-public modelNumber:AbstractControl;
-public maker:AbstractControl;
-public registrationNumber:AbstractControl;
-public engineNumber:AbstractControl;
-public chassisNumber:AbstractControl;
-public owner1:AbstractControl;
-public owner2:AbstractControl;
-public purchasePrice:AbstractControl;
-public salePrice:AbstractControl;
-public purchaseDate:AbstractControl;
-public saleDate:AbstractControl; 
-public color:AbstractControl;
+  public carStockForm: FormGroup;
 
-allAccounts:any[]=[];
-makerList =['Toyota','Honda','Hundai','Suzuki','Faw'];
+  allAccounts: AccountModel[] = [];
+  makerList = ['Toyota', 'Honda', 'Hundai', 'Suzuki', 'Faw'];
 
-  constructor(private fb:FormBuilder,private gu:GeneralHttpService) { 
+  constructor(private fb: FormBuilder, private gu: GeneralHttpService) {
+    this.createForm();
+  }
 
-    this.form=fb.group({
-      'name': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
-      'modelNumber': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
-      'color': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
-      'maker': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
-      'registrationNumber': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
-      'engineNumber': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
-      'chassisNumber': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
-      'owner1': ['', Validators.compose([Validators.required])],
-      'owner2': [''],
-      'purchaseDate': [''],
-      'saleDate': [''],
-      'purchasePrice': ['', Validators.compose([AmountValidator.validate])],
-      'salePrice': ['', Validators.compose([AmountValidator.validate])],
-      
-
+  createForm() {
+    this.carStockForm = this.fb.group({
+      name: ['', Validators.required],
+      modelNumber: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      color: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      maker: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      registrationNumber: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      engineNumber: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      chassisNumber: ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+      owner1: ['', Validators.required],
+      owner2: [''],
+      purchaseDate: [''],
+      saleDate: [''],
+      purchasePrice: ['', Validators.compose([AmountValidator.validate])],
+      salePrice: ['', Validators.compose([AmountValidator.validate])],
     });
-
-    this.name=this.form.controls['name'];
-    this.modelNumber=this.form.controls['modelNumber'];
-    this.maker=this.form.controls['maker'];
-    this.registrationNumber=this.form.controls['registrationNumber'];
-    this.engineNumber=this.form.controls['engineNumber'];
-    this.chassisNumber=this.form.controls['chassisNumber'];
-    this.owner1=this.form.controls['owner1'];
-    this.owner2=this.form.controls['owner2'];
-    this.purchasePrice=this.form.controls['purchasePrice'];
-    this.salePrice=this.form.controls['salePrice'];
-    this.color=this.form.controls['color'];
-    this.purchaseDate=this.form.controls['purchaseDate'];
-    this.saleDate=this.form.controls['saleDate'];
-
-
   }
 
   ngOnInit() {
@@ -70,14 +43,19 @@ makerList =['Toyota','Honda','Hundai','Suzuki','Faw'];
   }
 
   getAllAccount() {
-    this.gu.getAllAccounts().subscribe(data=> { 
-      this.allAccounts=data.ResponseData; 
-      console.log(this.allAccounts)   
-    }, error=> {});
+    this.gu.getAllAccounts().subscribe(data => {
+      this.allAccounts = this.sortAllAccounts(data.ResponseData);
+    }, error => { console.log(error) });
   }
 
-  onSubmit(m){
+  onSubmit(m) {
     console.log(m);
+  }
 
+  sortAllAccounts(accouts) {
+    return accouts.sort((obj1, obj2) => {
+      if (obj1.Number == obj2.Number) return 0;
+      return (obj1.Number > obj2.Number) ? 1 : -1;
+    });
   }
 }
