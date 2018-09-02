40134e0acfa469747ada082d889e52b10828621a
thezeeshanasghar <thezeeshanasghar@gmail.com>
2018-09-02 09:33:47 +0500

    add server down message

--- src/app/pages/accounts/accounts.component.ts
+++ src/app/pages/accounts/accounts.component.ts
@@ -1,13 +1,20 @@
-import { AmountValidator } from './../../../assets/validators/amount.valdator';
-import { FormGroup, AbstractControl, FormBuilder, Validators, FormControl, FormsModule } from '@angular/forms';
-import { Http } from '@angular/http';
-import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
-import { TransactionModel } from './../../Models/Transaction.model';
-import { Component, OnInit, TemplateRef } from '@angular/core';
-import { GeneralHttpService } from '../../services/general-http.service';
-import { AccountModel } from '../../Models/account.model';
-import { BsModalService } from 'ngx-bootstrap';
-import { Router } from '@angular/router';
+import { AmountValidator } from "./../../../assets/validators/amount.valdator";
+import {
+  FormGroup,
+  AbstractControl,
+  FormBuilder,
+  Validators,
+  FormControl,
+  FormsModule
+} from "@angular/forms";
+import { Http } from "@angular/http";
+import { BsModalRef } from "ngx-bootstrap/modal/bs-modal-ref.service";
+import { TransactionModel } from "./../../Models/Transaction.model";
+import { Component, OnInit, TemplateRef } from "@angular/core";
+import { GeneralHttpService } from "../../services/general-http.service";
+import { AccountModel } from "../../Models/account.model";
+import { BsModalService } from "ngx-bootstrap";
+import { Router } from "@angular/router";
 
 export class searchModel {
   name: number;
@@ -15,10 +22,9 @@ export class searchModel {
   mobile: number;
 }
 
-
 @Component({
-  selector: 'app-accounts',
-  templateUrl: './accounts.component.html'
+  selector: "app-accounts",
+  templateUrl: "./accounts.component.html"
 })
 export class AccountsComponent implements OnInit {
   loading = false;
@@ -27,13 +33,11 @@ export class AccountsComponent implements OnInit {
     name: 0,
     cnic: 0,
     mobile: 0
-  }
+  };
 
-  filter = '';
-
+  filter = "";
 
   showSearch = true;
-  warningTrigger = false;
   allAccounts: AccountModel[] = [];
   selectedAccountTransactions: TransactionModel[] = [];
   singleUser: any;
@@ -42,12 +46,11 @@ export class AccountsComponent implements OnInit {
 
   editUserForm: FormGroup;
 
-  number = new FormControl('', [Validators.required]);
-  name = new FormControl('', [Validators.required]);
-  mobileNumber = new FormControl('', [Validators.required]);
-  cnic = new FormControl('', [Validators.required]);
-  address = new FormControl('', [Validators.required]);
-
+  number = new FormControl("", [Validators.required]);
+  name = new FormControl("", [Validators.required]);
+  mobileNumber = new FormControl("", [Validators.required]);
+  cnic = new FormControl("", [Validators.required]);
+  address = new FormControl("", [Validators.required]);
 
   public form: FormGroup;
   public userAccount: AbstractControl;
@@ -56,20 +59,32 @@ export class AccountsComponent implements OnInit {
   message: string = "";
   showSearchResult: boolean = false;
 
-  constructor(private router: Router, public fb: FormBuilder, private gu: GeneralHttpService, private modalService: BsModalService, private http: Http) {
-
+  constructor(
+    private router: Router,
+    public fb: FormBuilder,
+    private gu: GeneralHttpService,
+    private modalService: BsModalService,
+    private http: Http
+  ) {
     this.loading = true;
     this.ip = this.gu.ip;
     this.port = this.gu.port;
 
     this.form = fb.group({
-      'userAccount': ['', Validators.compose([Validators.required])],
-      'incomeAmount': ['', Validators.compose([Validators.required, AmountValidator.validate, Validators.minLength(3)])],
-      'description': ['', Validators.compose([Validators.required, Validators.minLength(10)])]
-
+      userAccount: ["", Validators.compose([Validators.required])],
+      incomeAmount: [
+        "",
+        Validators.compose([
+          Validators.required,
+          AmountValidator.validate,
+          Validators.minLength(3)
+        ])
+      ],
+      description: [
+        "",
+        Validators.compose([Validators.required, Validators.minLength(10)])
+      ]
     });
-
-
 
     this.userAccount = this.form.controls["userAccount"];
     this.incomeAmount = this.form.controls["incomeAmount"];
@@ -87,12 +102,10 @@ export class AccountsComponent implements OnInit {
   modalRefExpense: BsModalRef;
   modalRefIncome: BsModalRef;
   setAlertOff() {
-    this.warningTrigger = false;
     this.search.mobile = 0;
     this.search.cnic = 0;
     this.search.name = 0;
   }
-
 
   showSearchResultToggle() {
     this.getAllAccounts();
@@ -102,7 +115,6 @@ export class AccountsComponent implements OnInit {
 
   searchUser(word) {
     // console.log(word);
-
 
     this.showSearchResult = true;
     let accounts: any[] = [];
@@ -115,14 +127,13 @@ export class AccountsComponent implements OnInit {
         console.log(e);
         accounts.push(e);
       }
-
     });
 
     this.allAccounts = accounts;
   }
 
   makeImageUrl(url) {
-    console.log(url)
+    console.log(url);
     return "http://{{ip}}:{{port}}/" + url;
   }
   openModalExpense(template: TemplateRef<any>) {
@@ -136,30 +147,39 @@ export class AccountsComponent implements OnInit {
   }
 
   getAllAccounts() {
-    this.gu.getAllAccounts().subscribe(data => {
-      this.allAccounts = data.ResponseData;
-      this.loading = false;
-      // console.log(this.allAccounts);
-    }, error => {
-
-
-
-    });
+    this.gu.getAllAccounts().subscribe(
+      data => {
+        this.allAccounts = data.ResponseData;
+        this.loading = false;
+        // console.log(this.allAccounts);
+      },
+      err => {
+        if (err.status == 0) {
+          this.loading = false;
+          this.message = "Unable to connect to the backend server";
+        }
+      }
+    );
   }
 
   getTransactionsByAccountId(id) {
-    this.gu.getTransactionsIdBy(id).subscribe(data => {
-      this.selectedAccountTransactions = data.ResponseData;
-      console.log(this.selectedAccountTransactions);
-      this.gu.getAccountById(id).subscribe(data => {
-        console.log(data);
-        this.singleUser = data.ResponseData;
-      }, error => { });
-
-    }, error => { console.log(error); });
+    this.gu.getTransactionsIdBy(id).subscribe(
+      data => {
+        this.selectedAccountTransactions = data.ResponseData;
+        console.log(this.selectedAccountTransactions);
+        this.gu.getAccountById(id).subscribe(
+          data => {
+            console.log(data);
+            this.singleUser = data.ResponseData;
+          },
+          error => {}
+        );
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
-
-
 
   getRowTotalUsingIndex(index: number): number {
     let sum = 0;
@@ -169,39 +189,38 @@ export class AccountsComponent implements OnInit {
     return sum;
   }
 
-
   onSubmit(form: searchModel) {
     console.log(form);
 
     if (form.name == 0 && form.cnic == 0 && form.mobile == 0) {
-      this.warningTrigger = true;
-      this.message = "Please select any one of given filters below." + "     " + "برائے مہربانی سرچ کے لیا کوئی ایک فلٹر کا انتخاب کریں"
-      console.log("empty form")
-    }
-
-    else if ((form.name !== 0 && form.cnic !== 0) || (form.name !== 0 && form.mobile !== 0) || (form.mobile !== 0 && form.cnic !== 0)) {
-
-      this.message = "Please select any one of given filters below." + "     " + "برائے مہربانی سرچ کے لیا کوئی ایک فلٹر کا انتخاب کریں"
-      this.warningTrigger = true;
-
+      this.message =
+        "Please select any one of given filters below." +
+        "     " +
+        "برائے مہربانی سرچ کے لیا کوئی ایک فلٹر کا انتخاب کریں";
+      console.log("empty form");
+    } else if (
+      (form.name !== 0 && form.cnic !== 0) ||
+      (form.name !== 0 && form.mobile !== 0) ||
+      (form.mobile !== 0 && form.cnic !== 0)
+    ) {
+      this.message =
+        "Please select any one of given filters below." +
+        "     " +
+        "برائے مہربانی سرچ کے لیا کوئی ایک فلٹر کا انتخاب کریں";
     }
 
     if (form.name !== 0) {
-      this.searchUser(form.name)
+      this.searchUser(form.name);
+    } else if (form.cnic !== 0) {
+      this.searchUser(form.cnic);
+    } else if (form.mobile !== 0) {
+      this.searchUser(form.mobile);
     }
-    else if (form.cnic !== 0) {
-      this.searchUser(form.cnic)
-    }
-    else if (form.mobile !== 0) {
-      this.searchUser(form.mobile)
-    }
-
 
     setTimeout(() => {
       this.search.mobile = 0;
       this.search.cnic = 0;
       this.search.name = 0;
-      this.warningTrigger = false;
-    }, 5000)
+    }, 5000);
   }
 }
